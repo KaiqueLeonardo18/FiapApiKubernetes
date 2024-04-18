f9fcfe33e355b3a42fe6f3c80007ae7eb44e656b
Kaique Leonardo <kaiqueleonardo18@gmail.com>
2024-04-18 10:04:20 -0300

    ajuste

--- FiapApiDocker/Controllers/WeatherForecastController.cs
+++ FiapApiDocker/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FiapApiDocker.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezingaaaaaaaaa", "Bracaaaaaaaaaaaaaaing", "Chilaaaaaaaaaaaaaaly", "Coolaaaaaaaaaaa", "Mildaaaaaaaaa", "Warmaaaaaaa", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
